0d3cf6b5cce8fa9afe455b4ea15e0d6b2585600a
Alex Speller <alex@alexspeller.com>
2020-01-16 18:55:26 +0000

    Mark association as loaded when preloading

--- lib/jit_preloader/active_record/associations/preloader/association.rb
+++ lib/jit_preloader/active_record/associations/preloader/association.rb
@@ -48,8 +48,9 @@ module JitPreloader
 
     def associate_records_to_owner(owner, records)
       association = owner.association(reflection.name)
+      association.loaded!
       if reflection.collection?
-        association.target ||= records
+        association.target = records if association.target.blank?
       else
         association.target ||= records.first
       end
